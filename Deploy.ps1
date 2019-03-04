19db7ebe818fc5d889d0467e6aca4e9839214b12
Tobias Böker <tb@softwarepioniere.de>
2019-03-04 16:24:42 +0100

    updated to cake.build

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,4 +1,35 @@
-#script to deploy with appveyor
-$env:PSModulePath = $env:PSModulePath + ";$(Get-Location)"
-Update-ModuleManifest -Path .\EventStoreUtil\EventStoreUtil.psd1 -ModuleVersion $env:APPVEYOR_BUILD_VERSION
-Publish-Module -Name EventStoreUtil -NuGetApiKey $env:PS_GALLERY_API_KEY
+[CmdletBinding()]
+Param(
+    [string]$version = "9.9.9",
+    [string]$apiKey
+)
+
+$moduleDir = "$(Get-Location)\xxModules"
+
+if (!(Test-Path -Path $moduleDir)) {
+    Write-Host "Creating Temp Modules Dir $moduleDir"
+    New-Item -Path $moduleDir -ItemType Directory
+}
+
+$mod = Import-PowerShellDataFile ./EventStoreUtil/EventStoreUtil.psd1
+
+foreach ($m in $mod.RequiredModules) {
+    Write-Host "Saving Module" $m.ModuleName $m.ModuleVersion
+    
+    $path ="$moduleDir\$($m.ModuleName)\$($m.ModuleVersion)"
+    Write-Host "Local Path" $path
+    if (!(Test-Path -Path $path)) {
+        Save-Module -Name $m.ModuleName -RequiredVersion $m.ModuleVersion -Path $moduleDir
+    }
+}
+
+$env:PSModulePath = "$env:PSModulePath;$moduleDir";
+Write-Host "Changed ModulePath" $env:PSModulePath
+
+Write-Host "Updating Module Manifest... with Version: $version"
+Update-ModuleManifest -Path .\EventStoreUtil\EventStoreUtil.psd1 -ModuleVersion $version
+
+if ($apiKey) {
+    Write-Host "Publishing Module... "
+    Publish-Module -Path .\EventStoreUtil -NuGetApiKey $apiKey
+}
